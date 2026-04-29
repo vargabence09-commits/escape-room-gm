7f4eb012a8631ff56056f01a0966707430b8c11d
vargabence09-commits <varga.bence.09@gmail.com>
2026-04-29 13:31:30 +0200

    update quest UUIDs

--- server.js
+++ server.js
@@ -14,15 +14,15 @@ const ROOM_PATHS = {
   'A Maja Birodalom': '/quests/4369dd01-bffc-4a75-932b-dced322cb5a7',
   'A Mélység Titka': '/quests/8ddda5f6-39b9-4b3e-85f7-cafa1d485de4',
   'Alien vs Predator': '/quests/c41cacdd-8067-4b20-ab6f-3628ed01bf69',
-  'Azték': '/quests/ae0a8127-ffe2-4da3-af21-c694dc35bebc',
-  'Bomb': '/quests/dce93e74-1818-4623-b360-799227e60f56',
-  'Éjszaka az Egyiptomi Múzeumban': '/quests/7256a480-b92a-47fc-be8f-c0d25bb0d726',
+  'Azték': '/quests/7256a480-b92a-47fc-be8f-c0d25bb0d726',
+  'Bomb': '/quests/ae0a8127-ffe2-4da3-af21-c694dc35bebc',
+  'Éjszaka az Egyiptomi Múzeumban': '/quests/5a5f2010-b6af-419b-85d5-5a25af92f08a',
   'Japan': '/quests/30d467ad-a606-48ad-a5af-6d21628f6ee3',
   'Madness': '/quests/f15525e2-59a9-4acf-a6eb-17396850be7d',
   'Prison': '/quests/821834ab-b031-46dc-8d3a-b1068e783b3c',
   'Roxfort és a Legendás Állatok': '/quests/6f104097-f8b7-4871-8619-668e4a00ee10',
   'Tetthely': '/quests/67fab808-7a23-411f-83fc-08f025a8f2cf',
-  'Zombie': '/quests/5a5f2010-b6af-419b-85d5-5a25af92f08a',
+  'Zombie': '/quests/b4351eaf-08ee-477b-bf04-6c8d04ba21bb',
 };
 
 if (!API_KEY) {
